6a7412b6b2fb65dfd0b36f7de327a1686e824cec
Apeksha Hegde <apeksha05hegde@gmail.com>
2024-01-10 00:24:04 +0530

    new

--- web_streamlit.py
+++ web_streamlit.py
@@ -1,21 +1,28 @@
 import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-import numpy as np
 
-
-st.title("Prospensity Score calculator")
+st.title("Propensity Score Calculator")
 uploaded_file = st.file_uploader("Choose a file")
+
 if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
+
+    df = pd.read_csv(uploaded_file)
+
+    X = df.drop('SGLT2', axis=1)
+    y = df['SGLT2']
 
 
-X=df.drop('SGLT2',axis=1)
-y=df['SGLT2']
-model=LogisticRegression()
-result=model.fit(X,y)
-df['Prospensity Score']= model.predict_proba(X)[:, 1]
+    model = LogisticRegression()
+    model.fit(X, y)
 
-st.write(df)
-df.to_csv('new_data.csv',index=False)
 
+    df['Propensity Score'] = model.predict_proba(X)[:, 1]
+
+    st.write(df)
+    st.download_button(label="Download data as CSV", 
+                       data=df.to_csv(index=False),
+                       file_name='new_data.csv',
+                       mime='text/csv')
+else:
+    st.write("Please upload a CSV file to proceed.")
